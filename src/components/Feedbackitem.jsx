9ab7881fda673d3e9c9b7057e5d37bb915072be2
Andre <andregozubuyuk@gmail.com>
2023-05-09 21:59:08 +0300

    Updated files

--- src/components/Feedbackitem.jsx
+++ src/components/Feedbackitem.jsx
@@ -6,8 +6,8 @@ function FeedbackItem({ item, handleDelete }) {
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
-      <button onClick={() => handleDelete(item.id)}>
-        <FaTimes color="purple"></FaTimes>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color="purple" />
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
